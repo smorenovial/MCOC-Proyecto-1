8a55b1d4f5d89bd98295f9cc45a193284fa8d24f
rcruz <rcruz@miuandes.cl>
2019-08-23 10:57:50 -0400

    Lista con valores de calores en distintos sensores

--- Entrega 6/Codigo_3d.py
+++ Entrega 6/Codigo_3d.py
@@ -1,13 +1,7 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Aug 21 14:14:40 2019
-
-@author: piedi
-"""
-
 from matplotlib.pylab import *
 from scipy.interpolate import interp1d
 import numpy as np
+import math
 
  
 a = 1.          #Ancho del dominio
@@ -111,14 +105,19 @@ framenum = 0
 graficocentro = []
 # Se fija un valor de 60 de tal forma de que arroje mas imagenes
 
-contador=0
-lista=[]
-lista2=[]
-while contador<10:
-    lista.append([1,1,1])
-    contador+=1
 
-for k in range(int32(11./dt)):
+sensor1=[]
+sensor2=[]
+sensor3=[]
+sensor4=[]
+sensor5=[]
+sensor6=[]
+sensor7=[]
+sensor8=[]
+sensor9=[]
+
+
+for k in range(int32(10./dt)):
     t = dt*(k+1)
     print "k = ", k, " t = ", t
  
@@ -127,42 +126,24 @@ for k in range(int32(11./dt)):
     u_k[0,:,:] = 20.
     u_k[-1,:,:] = 20.
     
-    print "Calor en punto [29,29,29]:", u_k[29,29,29]
+
     # Se fija la funcion de variacion de calor en la cara superior 
     u_k[:,-1,:] = 20 + 10* sin((2* math.pi/24)*t )
 
     numeros=[1,2,3,4,5,6,7,8,9,10]
 
-    if k == 200: # Tiempo aproximado valor 1
-        lista2.append(u_k[29,29,29])
 
-    if k == 410: # Tiempo aproximado valor 2
-        lista2.append(u_k[29,29,29])
-
-    if k == 615: # Tiempo aproximado valor 3
-        lista2.append(u_k[29,29,29])
-
-    if k == 815: # Tiempo aproximado valor 4
-        lista2.append(u_k[29,29,29])
-
-    if k == 1015: # Tiempo aproximado valor 5
-        lista2.append(u_k[29,29,29])
-
-    if k == 1216: # Tiempo aproximado valor 6
-        lista2.append(u_k[29,29,29])
-
-    if k == 1417: # Tiempo aproximado valor 7
-        lista2.append(u_k[29,29,29])
-
-    if k == 1618: # Tiempo aproximado valor 8
-        lista2.append(u_k[29,29,29])
-
-    if k == 1819: # Tiempo aproximado valor 9
-        lista2.append(u_k[29,29,29])
-
-    if k == 2020: # Tiempo aproximado valor 10
-        lista2.append(u_k[29,29,29])
-
+    # Cada 202 veces que avanza k, sube en 1 el valor de t aproximadamente
+    if k % 202: 
+        sensor1.append(u_k[29,29,29]) # Sensores 7
+        sensor2.append(u_k[29,15,29]) # Sensores 8
+        sensor3.append(u_k[29,0,29]) # Sensores 9
+        sensor4.append(u_k[15,15,15])
+        sensor5.append(u_k[15,29,15])
+        sensor6.append(u_k[15,0,15])
+        #sensor7.append(u_k[29,29,29])
+        #sensor8.append(u_k[29,15,29])
+        #sensor9.append(u_k[29,0,29])
 
     #Loop en el espacio   i = 1 ... n-1   u_km1[0] = 0  u_km1[n] = 20
     for i in range(1,Nx):
@@ -194,19 +175,26 @@ for k in range(int32(11./dt)):
  
     print "Tmax = ", u_k.max()
  
-print "Calores del punto [29,29,29], en funcion del tiempo (t):"
-print lista2
-print "Tiempos t:"
-print numeros
+#print "Calores del punto [29,29,29], en funcion del tiempo (t):"
+#print sensor1
+#print "Tiempos t:"
+#print numeros
 
-x = np.linspace(0, 10, num=11, endpoint=True)
-y = np.lista2(t)
-f = interp1d(x, y)
-f2 = interp1d(x, y, kind='cubic')
+# Se imprime el grafico
+plt.plot(numeros,sensor1)
+plt.plot(numeros,sensor2)
+plt.plot(numeros,sensor3)
+plt.plot(numeros,sensor4)
+plt.plot(numeros,sensor5)
+plt.plot(numeros,sensor6)
+#plt.plot(numeros,sensor7)
+#plt.plot(numeros,sensor8)
+#plt.plot(numeros,sensor9)
 
-xnew = np.linspace(0, 10, num=41, endpoint=True)
-plt.plot(x, y, 'o', xnew, f(xnew), '-', xnew, f2(xnew), '--')
-plt.legend(['data', 'linear', 'cubic'], loc='best')
+plt.title("Variacion de calor v/s Tiempo")
+plt.xlabel("Tiempo")
+plt.ylabel("Calor")
+
 plt.show()
 
 
